8e6a102ad943ef9798683c339b02ed6449fefb63
Michael Brooks <michael.brooks@det.nsw.edu.au>
2026-03-16 14:25:34 +1100

    Update app.js

--- simple-pwa/app.js
+++ simple-pwa/app.js
@@ -1,1 +1,6 @@
-
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/2026_web_dev/simple-pwa/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  }
